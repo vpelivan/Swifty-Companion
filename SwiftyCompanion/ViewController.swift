84408c660dce1f04c0d7cf65c68d47f8fc1958cd
vpelivan <pelivan.v.1988@gmail.com>
2019-10-25 18:32:19 +0300

    Storyboard First Version

---
+++ SwiftyCompanion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyCompanion
+//
+//  Created by Viktor PELIVAN on 10/25/19.
+//  Copyright © 2019 Viktor PELIVAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
